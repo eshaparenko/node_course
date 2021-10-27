711d393fe13a933d524d4ac9a03f6334d3877a40
Eugene <gesha8610@gmail.com>
2021-10-27 22:23:59 +0300

    task1

---
+++ README.md
@@ -0,0 +1,5 @@
+To run server:
+```
+npm install
+APP_PORT=3000 node app.js prod
+```
---
+++ config.js
@@ -0,0 +1,6 @@
+const PORT = process.env.APP_PORT;
+const ENV = process.argv.slice(2);
+
+module.exports = {
+    PORT, ENV
+}
